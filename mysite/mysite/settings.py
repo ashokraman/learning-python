cd79db105d3374beed6230bbc0707127e5db92bd
ashokraman <ashoknraman@gmail.com>
2016-01-28 19:35:56 +0530

    static root

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -123,4 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+print "base dir path", BASE_DIR
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
