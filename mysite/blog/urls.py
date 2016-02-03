25a1e83053c057cb966af000d227c7d7fe3b11a0
ashokraman <ashoknraman@gmail.com>
2016-02-03 13:26:21 +0530

    detail

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list')
+    # ex: /post/
+    url(r'^$', views.post_list, name='post_list'),
+    # ex: /post/5/
+    url(r'^(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
